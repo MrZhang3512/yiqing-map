5f342cec401e29b1155ecf95b784aee938ba1819
unknown <15235233600@sohu.com>
2020-10-06 16:05:00 +0800

    中国疫情地图

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,112 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <!-- 初始化echarts需要有个宽高的盒子 -->
+    <div ref="mapbox" style="height:600px;width:900px"></div>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+// 导入中国地图
+import "echarts/map/js/china"
+import jsonp from 'jsonp'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      list:[],
+      option:{
+      title:{
+        text:"疫情动态"
+      },
+      series:[{
+        name:"当前确诊人数",
+        type: 'map',
+        map: 'china',
+        zoom:1.1,//当前视角的缩放比例
+        roam:true,//鼠标滚轮放大缩小
+        label:{
+          //控制对应地区的汉字
+          show:true,
+          color:"#333",
+          fontSize:10
+        },
+        itemStyle:{
+          //控制对应地区的样式
+          areaColor:"#eee",
+          // borderColor:"blue"
+        },
+        emphasis:{
+          //控制鼠标滑过后
+          label:{
+          color:"#fff",
+          fontSize:12
+        },
+        itemStyle:{
+          areaColor:"#83b5e7",
+        }
+        },
+        data:[]
+        //用来展示后台传来的数据
+
+      }],
+      visualMap:[{
+         type: 'piecewise', // 定义为分段型 visualMap
+         splitNumber:5,//分为几段
+         pieces: [
+           //分段
+            {min: 10000}, // 不指定 max，表示 max 为无限大（Infinity）。
+            {min: 1000, max: 9999},
+            {min: 100, max: 999},
+            {min: 10, max: 99},
+            {min: 1, max: 9}
+        ],
+        // align:"right"//默认为左
+      }],
+      tooltip:{
+        trigger:'item'//鼠标移动到的区域显示数据
+      },
+      toolbox:{
+        //左侧显示功能栏
+        show: true,
+        orient: 'vertical',
+        left: 'right',
+        top: 'center',
+        feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+      }      
+    }
+    }
+  },
+  methods:{
+    //请求数据
+    getData(){
+      jsonp('https://interface.sina.cn/news/wap/fymap2020_data.d.json?_=1580892522427',
+            {},
+            (err,data)=>{
+              if(!err){
+                //代表请求数据成果
+                console.log(data)
+                let list1=data.data.list.map(item=>({name:item.name,value:item.econNum}))
+                this.option.series[0].data=list1
+                 console.log(list1)
+                this.e.setOption(this.option)
+  
+              }
+            })
+    }
+  },
+  mounted(){
+    //获取数据
+    this.getData()
+    //初始化一个echarts
+    this.e=echarts.init(this.$refs.mapbox)
+    //设置option
+    this.e.setOption(this.option)
   }
+  
 }
 </script>
 
